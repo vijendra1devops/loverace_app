17845e3377770b8301a97c4047434388c061e3ae
vijendra <vijendra.devops@gmail.com>
2026-05-10 19:35:27 +0530

    loverace_ provider_updated

--- terraform_Infra/environment/dev/provider.tf
+++ terraform_Infra/environment/dev/provider.tf
@@ -1,24 +1,25 @@
 terraform {
+  required_version = ">= 1.9.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.70.0"
+      version = "~> 3.117"
     }
-    # random = {
-    #   source  = "hashicorp/random"
-    #   version = "4.70.0"
-    # }
   }
+
+  # random = {
+  #   source  = "hashicorp/random"
+  #   version = "4.70.0"
+  # }
+
   backend "azurerm" {
     resource_group_name  = "backend-rg"        # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "devbackendstg"     # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "my-backend-cnt"    # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
-
 }
-
-
 provider "azurerm" {
   features {}
   subscription_id = var.ARM_SUBSCRIPTION_ID
