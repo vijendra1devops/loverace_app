9155d6097854b52c025be06192767667a8253f16
vijendra <vijendra.devops@gmail.com>
2026-05-10 18:21:25 +0530

    loverace_ infra.yml corrected

--- terraform_Infra/environment/dev/provider.tf
+++ terraform_Infra/environment/dev/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.70.0"
+      version = "4.38.0"
     }
     # random = {
     #   source  = "hashicorp/random"
-    #   version = "4.7.0"
+    #   version = "4.38.0"
     # }
      }
    backend "azurerm" {
