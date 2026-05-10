7fec2407a4dfdf9523ccb6b78c8ed81b549e1a67
vijendra <vijendra.devops@gmail.com>
2026-05-10 17:16:41 +0530

    loverace src code & terraform infra code added

---
+++ terraform_Infra/environment/dev/main.tf
@@ -0,0 +1,80 @@
+module "module-rg" {
+  source = "../../resource_modules/azurerm-resource_group"
+  rgs    = var.dev-rgs
+}
+
+module "module-app-service_plans" {
+  depends_on = [ module.module-rg ]
+  source = "../../resource_modules/azurerm-service_plan"
+  service_plans = var.dev-service_plans
+}
+
+module "module-webapp" {
+  depends_on = [ module.module-rg, module.module-app-service_plans ]
+  source = "../../resource_modules/azurerm-webapp"
+  webapps = var.dev-webapps
+}
+module "module-vnets" {
+  depends_on = [module.module-rg]
+  source     = "../../resource_modules/azurerm-vnets"
+  vnets      = var.dev-vnets
+
+}
+# module "module-nsgs" {
+#   depends_on = [ module.module-rg ]
+#   source = "../../resource_modules/azurerm-nsgs"
+#   nsgs = var.dev-nsgs
+# }
+# module "module-asgs" {
+#   depends_on = [ module.module-rg ]
+#   source = "../../modules/azurerm-asgs"
+#   asgs = var.dev-asgs
+# }
+# module "module-win-vms" {  
+#   depends_on = [ module.module-vnets, module.module-keyvaults ] #module.var.dev-asgs 
+#   source = "../../resource_modules/azurerm-win-vms"
+#   window-vms = var.dev-win-vms
+# }
+# module "module-bastions" {
+#   depends_on = [ module.module-vnets ]
+#   source = "../../modules/azurerm-bastions"
+#   bastions = var.dev-bastions
+# }
+# module "module-keyvaults" {
+#   depends_on = [ module.module-rg ]
+#   source = "../../resource_modules/azurerm-keyvault"
+#   keyvaults = var.dev-keyvaults
+# }
+
+# module "module-linux-vms" {
+  
+#   depends_on = [ module.module-vnets,module.module-keyvaults ]#module.var.dev-asgs 
+#   source = "../../modules/azurerm-linux-vms"
+#   linux-vms=var.dev-linux-vms
+# }
+module "module-acr" {
+  depends_on = [module.module-rg]
+  source     = "../../resource_modules/azurerm_acr"
+  acr        = var.dev-acr
+
+}
+module "module-aks-cluster" {
+  depends_on   = [module.module-rg]
+  source       = "../../resource_modules/azurerm_aks"
+  aks-clusters = var.dev-aks-clusters
+
+}
+
+# module "module-mssql-server-with-db" {
+#   depends_on           = [module.module-rg]
+#   source               = "../../resource_modules/azurerm_mssql_server"
+#   mssql-server-with-db = var.dev-mssql-server-with-db
+# }
+
+module "postgrssqlserver" {
+  depends_on = [ module.module-vnets ]
+  source = "../../resource_modules/azurerm_postgrey_server"
+  postgresql-server-with-db = var.dev-postgresql-server-with-db
+
+  
+}
---
+++ terraform_Infra/environment/dev/variable.tf
@@ -0,0 +1,87 @@
+variable "dev-rgs" {}
+variable "dev-service_plans" {}
+variable "dev-webapps" {}
+variable "dev-vnets" {}
+# variable "dev-nsgs" {}
+# variable "dev-win-vms" {}
+# variable "dev-keyvaults" {}
+# variable "ARM_CLIENT_ID" {
+#   description = "Azure Service Principal Client ID"
+#   type        = string
+#   sensitive   = true
+# }
+
+# variable "ARM_CLIENT_SECRET" {
+#   description = "Azure Service Principal Client Secret"
+#   type        = string
+#   sensitive   = true
+# }
+
+# variable "ARM_TENANT_ID" {
+#   description = "Azure Tenant ID"
+#   type        = string
+#   sensitive   = true
+# }
+
+variable "ARM_SUBSCRIPTION_ID" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+variable "dev-acr" {}
+
+variable "dev-aks-clusters" {
+     description = "Map of AKS clusters configuration."
+  type = map(object({
+    # Network configuration
+    vnet_name           = string
+    vnet_address_space  = list(string)
+    location            = string
+    resource_group_name = string
+    tags                = map(string)
+    subnets = map(object({
+      subnet_name             = string
+      subnet_address_prefixes = list(string)
+    }))
+    acr_name = string
+    # Public IP configuration
+    pip_name          = string
+    allocation_method = string
+    sku               = string
+    # Application Gateway configuration
+    cluster_name                  = string
+    app_gway_name                  = string
+    sku_tier                       = string
+    sku_name                       = string
+    capacity                       = number
+    gateway_ip_configuration       = string
+    frontend_port_name             = string
+    port                           = number
+    frontend_ip_configuration_name = string
+    backend_address_pool_name      = string
+    backend_http_settings_name     = string
+    cookie_based_affinity          = string
+    protocol                       = string
+    request_timeout                = number
+    request_routing_rule           = string
+    priority                       = number
+    rule_type                      = string
+    http_listener_name             = string
+    # AKS Cluster configuration
+    default_node_pools     = map(object({
+      node_pool_name=string
+    vm_size        = string
+    node_count     = number
+    }))
+    
+  #network profile
+      dns_prefix     = string
+    agis_name      = string
+    network_plugin = string
+    network_policy = string
+    service_cidr   = string
+    dns_service_ip = string
+  }))
+}
+
+# variable "dev-mssql-server-with-db" {}
+variable "dev-postgresql-server-with-db" {}
